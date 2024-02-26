e577af97cc9b1add5bc7652b7ebaff1b9308caa9
Ivan Titov <titov.i@tytocare.com>
2024-02-26 14:05:28 +0200

    Improved binding in BaseFragment.

--- app/src/main/java/com/kontvip/wisecoin/presentation/core/BaseFragment.kt
+++ app/src/main/java/com/kontvip/wisecoin/presentation/core/BaseFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.kontvip.wisecoin.databinding.ActivityMainBinding
+import java.lang.reflect.Method
 import java.lang.reflect.ParameterizedType
 
 abstract class BaseFragment<B : ViewBinding> : Fragment() {
@@ -17,22 +18,29 @@ abstract class BaseFragment<B : ViewBinding> : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        val bindingClass = extractGenericType(javaClass)
-        val method = bindingClass.getDeclaredMethod(
-            "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
-        )
-        _binding = method.invoke(null, layoutInflater, null, false) as B
+        val inflateMethod = extractBindingInflateMethod(javaClass)
 
+        _binding = inflateMethod.invoke(null, layoutInflater, null, false) as B
         return binding.root
     }
 
-    private fun extractGenericType(clazz: Class<*>): Class<*> {
+
+    private fun extractBindingInflateMethod(clazz: Class<*>): Method {
         val superClass = clazz.genericSuperclass
         return try {
-            val parameterizedType = superClass as ParameterizedType
-            parameterizedType.actualTypeArguments[0] as Class<*>
+            val parameterizedTypes = (superClass as ParameterizedType).actualTypeArguments
+
+            val bindingClass = parameterizedTypes.find {
+                (it as? Class<*>)?.declaredMethods?.any { method ->
+                    method.name == "inflate"
+                } == true
+            } as Class<*>
+
+            bindingClass.getDeclaredMethod(
+                "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
+            )
         } catch (e: Exception) {
-            extractGenericType(superClass as Class<*>)
+            extractBindingInflateMethod(superClass as Class<*>)
         }
     }
 
