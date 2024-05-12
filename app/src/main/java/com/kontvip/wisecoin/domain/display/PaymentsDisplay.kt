a5fb20c0f28f3ff846d56e326c911dd3854fdaf1
Ivan Titov <titov.i@tytocare.com>
2024-05-12 20:30:07 +0200

    Code refactoring.

--- app/src/main/java/com/kontvip/wisecoin/domain/display/PaymentsDisplay.kt
+++
@@ -1,7 +0,0 @@
-package com.kontvip.wisecoin.domain.display
-
-import com.kontvip.wisecoin.domain.core.UiState
-
-interface PaymentsDisplay : UiState.UiDisplay {
-    fun displayPayment(display: PaymentDisplay, position: Int)
-}
